84334cf4a0a52c289f9b4946e6241055c0e931ae
af1987 <97942066+af1987@users.noreply.github.com>
2026-08-20 12:33:10 +0200

    Add user command test header

---
+++ tests/user_commands_tests.hpp
@@ -0,0 +1,17 @@
+/*
+    user_commands_tests.hpp
+
+    v0.0.03:
+        - declare focused user class, !set parser and password hashing tests
+
+    Author: gpt-5.6-sol
+    Date: 2026-08-19
+*/
+
+#pragma once
+
+namespace dc24h::tests {
+
+void run_user_command_tests();
+
+}  // namespace dc24h::tests
